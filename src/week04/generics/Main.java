edecbf08d55e046b26850bd657fd199f6a01d550
ahmetmucahitinan <ahmt.mcht.inn@gmail.com>
2024-04-04 17:15:32 +0300

    Generic konusuna giriş yapıldı ve örnek çözümler üzerinden yapıldı

---
+++ src/week04/generics/Main.java
@@ -0,0 +1,11 @@
+package week04.generics;
+
+public class Main {
+    public static void main(String[] args) {
+        GenericClass<Integer> intObj = new GenericClass<>(5);
+        System.out.println("Generic Class returns : " + intObj.getData());
+
+        GenericClass<String> stringObj = new GenericClass<>("Java Programming");
+        System.out.println("Generics Class returns : " + stringObj.getData());
+    }
+}
