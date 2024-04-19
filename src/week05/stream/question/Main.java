a8c8ca1d5ca37f1a6541826147e615c15ce82ca5
Ahmet Mücahit İNAN <ahmt.mcht.inn@gmail.com>
2024-04-19 13:37:51 +0300

    Stream sorusu çözüldü

---
+++ src/week05/stream/question/Main.java
@@ -0,0 +1,39 @@
+package week05.stream.question;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    /*
+    * 1 -> Student sınıfı oluşturalım -> name,surname,grade;
+    *      1.1 -> Encapsulated.
+    * 2 -> Main sınıfında girilen sayı kadar student nesnesi üreten bir metot yazalım.
+           (grade kısmı 0-100 aralığında rastgele değer alsın.)
+    * 3 -> Daha sonra öğrencilerin not ortalamasını hesaplayıp dönen bir stream yazalım.
+    * 4 -> Notu 50' den düşük olan öğrencileri liste olarak dönen bir stream yazalım.
+    * 5 -> Öğrencilerin isimlerini ve öğrencilerin notlarını yazdıran bir stream yazalım.
+    * 6 -> Öğrencilerin isimleri ve öğrencilerin notlarını bir collection yapısına toplayalım
+    * */
+    public static void main(String[] args) {
+        List<Student> studentList =  Student.generateStudents(30);
+        Double averageGrade = Student.calculateAverageGrade(studentList);
+        List<Student> studentsBellowBound = studentList.stream()
+                .filter(x-> x.getGrade()<50).collect(Collectors.toList());
+        System.out.println(averageGrade);
+        studentsBellowBound.forEach(System.out::println);
+        studentList.forEach(x-> System.out.println(x.getName() + " " + x.getGrade()));
+
+        Map<String,Integer> studentsAndGrades = new HashMap<>();
+        studentsAndGrades = studentList.stream()
+                .collect(Collectors.toMap( k -> k.getName(), Student::getGrade));
+
+        studentsAndGrades.entrySet()
+                .forEach( entry -> System.out.println(
+                        "Ogrenci Adı : " + entry.getKey()+
+                                "\nOgrenci Notu : " + entry.getValue()+"\n"
+                ));
+    }
+}
